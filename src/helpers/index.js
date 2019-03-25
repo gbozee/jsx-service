39fa3101f6b6d1ab1a43309a91be5bf1182bbb20
Beesama <gbozee@gmail.com>
2019-03-25 18:58:04 +0100

    currently blocked

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,28 +1,160 @@
+function splitAndClean(text) {
+  return text
+    .split("\n")
+    .filter(Boolean)
+    .map(x => x.trim());
+}
 
 function createHTMLFromSchema(schema, props = {}) {
-
+  let text = removeDuplicates(schema);
 }
 
 function removeDuplicates({ files, resolutions }) {
-    let componentNames = Object.keys(resolutions)
-}
-function removeDefaultExport(){
+  let componentNames = Object.keys(resolutions);
+  let nonRoot = componentNames.filter(
+    (x, i) => i !== componentNames.indexOf("root")
+  );
+  let merged = nonRoot.map(component => {
+    let merger = resolutions[component].map(x => files[x]).join("\n");
+    return removeDefaultExport(merger);
+  });
+  let dependencies = resolutions.root;
+  if (nonRoot.length > 0) {
+    let mergingResolutions = [].concat(...nonRoot.map(x => resolutions[x]));
+    dependencies = [...new Set([...dependencies, ...mergingResolutions])];
+  }
+  // let transform = nonRoot.map((component, index) => {
+  //   let result = resolutions[component].map(x => {
+  //     let orderedImport = reOrderImports(merged[index], x);
+  //     let replacement = replaceImport(orderedImport, x, files[x]);
+  //     return replacement;
+  //   });
+  //   let reordered = reOrderImports(result.join("\n"));
+  //   return removeDuplicateImports(reordered);
+  // });
+  // if (transform.length > 0) {
+  //   debugger;
+  //   return;
+  // }
+  let rootJoin = dependencies.map(x => files[x]).join("\n");
+  rootJoin = removeDefaultExport(rootJoin, "\n");
+  let splitting = splitImportAndNonImport(rootJoin, dependencies);
+  let importPath = splitting.modulePathImport
+    .map(x => {
+      let i = dependencies.find(o => x.includes(o));
+      return files[i];
+    })
+    .join("\n");
+  debugger;
+  rootJoin = []
+    .concat(
+      ...splitting.notModulePathImport,
+      importPath,
+      splitting.withoutImport
+    )
+    .join("\n");
 
+  let orderedImport = [];
+  let finalText = rootJoin;
+  finalText = reOrderImports(finalText, dependencies);
+  dependencies.forEach((x, index) => {
+    let replacement = replaceImport(finalText, x, files[x], "\n");
+    finalText = replacement;
+    orderedImport.push(replacement.split("\n"));
+  });
+  // let orderedImport = dependencies.map((x, index) => {
+  //   let hmo = reOrderImports(rootJoin, x);
+  //   let replacement = replaceImport(hmo, x, files[x], "\n");
+  //   debugger;
+  //   return replacement.split("\n");
+  // });
+  let { withoutImport, notModulePathImport } = splitImportAndNonImport(
+    files.root,
+    dependencies
+  );
+  let merger = []
+    .concat(...orderedImport)
+    .concat(notModulePathImport, withoutImport);
+  let reordered = reOrderImports(merger.join("\n"), dependencies);
+  let result = removeDuplicateImports(reordered);
+  return result;
 }
-function removeNewlines(){
+function removeNewlines(text, delimiter = "") {
+  let result = splitAndClean(text).join(delimiter);
+  return result;
+}
+function removeDefaultExport(text, delimiter = "") {
+  let lineArray = splitAndClean(text);
+  let remaining = lineArray.filter(line => !line.includes("export default"));
+  return removeNewlines(remaining.join("\n"), delimiter);
+}
+function replaceImport(
+  textContent,
+  modulePath,
+  moduleContent,
+  delimiter = "\n"
+) {
+  let orderedImportContent = reOrderImports(textContent, modulePath);
+  let cleanModuleContent = removeDefaultExport(moduleContent, delimiter);
+  let {
+    notModulePathImport,
+    modulePathImport,
+    withoutImport
+  } = splitImportAndNonImport(orderedImportContent, modulePath);
+  let newModulePathImport = modulePathImport.map(() => cleanModuleContent);
+  let result = notModulePathImport
+    .concat(newModulePathImport)
+    .concat(withoutImport)
+    .join(delimiter);
+  return result;
+}
+function removeDuplicateImports(text, delimiter = "\n") {
+  let { importLines, withoutImport } = splitImportAndNonImport(text);
+  let noDuplicate = [...new Set(importLines)].concat(withoutImport);
+  let result = noDuplicate.join(delimiter);
+  return result;
+}
+function reOrderImports(text, modulePath) {
+  let {
+    notModulePathImport,
+    modulePathImport,
+    withoutImport
+  } = splitImportAndNonImport(text, modulePath);
+  return notModulePathImport
+    .concat(modulePathImport)
+    .concat(withoutImport)
+    .join("\n");
+}
+function splitImportAndNonImport(text, modulePath) {
+  let lineArray = splitAndClean(text);
 
-}
-function replaceImport(){
-
-}
-function removeDuplicateImports(){
-    
+  let importLines = lineArray.filter(x => x.includes("import"));
+  let notModulePathImport = importLines.filter(x => {
+    if (Array.isArray(modulePath)) {
+      return !modulePath.some(m => x.includes(m));
+    }
+    return !x.includes(modulePath);
+  });
+  let modulePathImport = importLines.filter(x => {
+    if (Array.isArray(modulePath)) {
+      return modulePath.some(m => x.includes(m));
+    }
+    return x.includes(modulePath);
+  });
+  let withoutImport = lineArray.filter(x => !importLines.includes(x));
+  return {
+    importLines,
+    notModulePathImport,
+    modulePathImport,
+    withoutImport
+  };
 }
 export default {
-    createHTMLFromSchema,
-    removeDuplicates,
-    removeDefaultExport,
-    removeNewlines,
-    replaceImport,
-    removeDuplicateImports
-}
+  createHTMLFromSchema,
+  removeDuplicates,
+  removeDefaultExport,
+  removeNewlines,
+  replaceImport,
+  removeDuplicateImports,
+  reOrderImports
+};
--- tests/test_multiple_files.js
+++ tests/test_multiple_files.js
@@ -1,17 +1,15 @@
 const expect = require("chai").expect;
-const {
-    helpers
-} = require("../index");
+const { helpers } = require("../index");
 
-describe("Multiple components depending on each other", function () {
-    let utils = `
+describe("Multiple components depending on each other", function() {
+  let utils = `
     import React from 'react';
     import htm from 'html';
     const html = htm.bind(React.createElement)
 
     export default html;
-    `
-    let Text = `
+    `;
+  let Text = `
     import React from 'react';
     import html from "./utils";
     
@@ -21,11 +19,11 @@ describe("Multiple components depending on each other", function () {
     function Text(props){
         return html\`<p ...\${props}/>\`
     }
-    export default Text`
+    export default Text`;
 
-    let index = `
+  let index = `
     import React from 'react';
-    import html from './utils';
+    import html from "./utils";
     import Text, {Heading} from "./Text";
 
     export default function (){
@@ -34,43 +32,108 @@ describe("Multiple components depending on each other", function () {
             <\${Text}>This is the text content</\${Text}>
         </div>\`
     }
-    `
-    let result = `
+    `;
+  let result = `
     import React from 'react';
     import html from "./utils";
-    
     export function Heading(props){
         return html\`<h1 ...\${props}/>\`
     }
-
     function Text(props){
         return html\`<p ...\${props}/>\`
     }
-
     export default function (){
         return html\`<div>
             <\${Heading}>This is the heading</\${Heading}>
             <\${Text}>This is the text content</\${Text}>
         </div>\`
     }
-    `
-    describe("merging Files", () => {
-        it('should remove `export default` line from files', () => {
-            expect(helpers.removeDefaultExport(utils)).to.equal(
-                helpers.removeNewlines(
-                    `import React from 'react';
+    `;
+  describe("merging Files", () => {
+    it("should remove `export default` line from files", () => {
+      expect(helpers.removeDefaultExport(utils)).to.equal(
+        helpers.removeNewlines(
+          `import React from 'react';
                     import htm from 'html';
                     const html = htm.bind(React.createElement)`
-                )
-            )
-        })
-        describe("Imports", () => {
-            let mergedResult = `
+        )
+      );
+    });
+    it("should reorder the imports to the bottom.", () => {
+      expect(
+        helpers.removeNewlines(
+          helpers.reOrderImports(
+            `
+            import React from 'react';
+            import utils from "./utils";
+            import htm from 'html';
+            const html = html.bind(React.createElement)`,
+            "./utils"
+          )
+        )
+      ).to.equal(
+        helpers.removeNewlines(
+          `import React from 'react';
+              import htm from 'html';
+              import utils from "./utils";
+              const html = html.bind(React.createElement)`
+        )
+      );
+    });
+    it("should reorder imports for more advanced scenarios", () => {
+      expect(
+        helpers.removeNewlines(
+          helpers.reOrderImports(
+            `import React from 'react';
+          import utils from "./utils";
+          import htm from 'html';
+          import {RedZone} from "./utils";
+          import styled from 'styled-components';
+          const html = html.bind(React.createElement)`,
+            "./utils"
+          )
+        )
+      ).to.equal(
+        helpers.removeNewlines(
+          `import React from 'react';
+            import htm from 'html';
+            import styled from 'styled-components';
+            import utils from "./utils";
+            import {RedZone} from "./utils";
+            const html = html.bind(React.createElement)`
+        )
+      );
+    });
+    describe("Imports", () => {
+      let mergedResult = `
             import React from 'react';
             import React from 'react';
             import htm from 'html';
             const html = htm.bind(React.createElement)
-        
+            export function Heading(props){
+                return html\`<h1 ...\${props}/>\`
+            }
+            function Text(props){
+                return html\`<p ...\${props}/>\`
+            }
+            export default Text`;
+      it("should replace `import` of a file with actual content", () => {
+        expect(
+          helpers.removeNewlines(
+            helpers.replaceImport(Text, "./utils", utils),
+            "\n"
+          )
+        ).to.equal(helpers.removeNewlines(mergedResult, "\n"));
+      });
+      it("should remove duplicate imports", () => {
+        expect(
+          helpers.removeNewlines(helpers.removeDuplicateImports(mergedResult))
+        ).to.equal(
+          helpers.removeNewlines(
+            `
+            import React from 'react';
+            import htm from 'html';
+            const html = htm.bind(React.createElement)
             export function Heading(props){
                 return html\`<h1 ...\${props}/>\`
             }
@@ -78,58 +141,44 @@ describe("Multiple components depending on each other", function () {
                 return html\`<p ...\${props}/>\`
             }
             export default Text`
-            it('should replace `import` of a file with actual content', () => {
-                expect(helpers.replaceImport(Text, "./utils")).to.equal(
-                    helpers.removeNewlines(mergedResult)
-                )
-            })
-            it('should remove duplicate imports', () => {
-                expect(helpers.removeDuplicateImports(mergedResult)).to.equal(
-                    helpers.removeNewlines(`
-                    import React from 'react';
-                    import htm from 'html';
-                    const html = htm.bind(React.createElement)
-                
-                    export function Heading(props){
-                        return html\`<h1 ...\${props}/>\`
-                    }
-                    function Text(props){
-                        return html\`<p ...\${props}/>\`
-                    }
-                    export default Text`)
-                )
-            })
-
-        })
-        it("should remove duplicate dependencies", () => {
-            expect(helpers.removeDuplicates({
-                files: {
-                    root: index,
-                    "./Text": Text
-                },
-                resolutions: {
-                    root: ["./Text"]
-                }
-            })).to.equals(result)
-        })
-    })
-    describe("generating HTML", () => {
-        it("When all dependencies are relative to each other", () => {
-            let schema = {
-                files: {
-                    "./Text": Text,
-                    "./utils": utils,
-                    root: index
-                },
-                resolutions: {
-                    root: ["./Text", "./utils"],
-                    './Text': ["./utils"]
-                }
+          )
+        );
+      });
+    });
+    it("should remove duplicate dependencies", () => {
+      expect(
+        helpers.removeNewlines(
+          helpers.removeDuplicates({
+            files: {
+              root: index,
+              "./Text": Text
+            },
+            resolutions: {
+              root: ["./Text"]
             }
-            helpers.createHTMLFromSchema(schema).then(content => {
-                expect(content).to.equal("<div><h1>This is the heading</h1><p>this is the text content</p></div>")
-            })
-        })
-
-    })
-})
+          })
+        )
+      ).to.equals(helpers.removeNewlines(result));
+    });
+  });
+  describe("generating HTML", () => {
+    it("When all dependencies are relative to each other", () => {
+      let schema = {
+        files: {
+          "./Text": Text,
+          "./utils": utils,
+          root: index
+        },
+        resolutions: {
+          root: ["./Text", "./utils"],
+          "./Text": ["./utils"]
+        }
+      };
+      helpers.createHTMLFromSchema(schema).then(content => {
+        expect(content).to.equal(
+          "<div><h1>This is the heading</h1><p>this is the text content</p></div>"
+        );
+      });
+    });
+  });
+});
